e6caddc30204184f0e280f9cf8acab650df78ed0
Sergio <smg0055@alu.medac.es>
2025-10-20 14:26:02 +0200

    LISTAS

---
+++ 1Programacion/tarea1 - copia.java
@@ -0,0 +1,6 @@
+public class tarea1 {
+    public static void main(String[] args) {
+        string name = "nombre"; 
+        System.out.println(name);
+    }
+}
--- 1Programacion/tarea1.java
+++ 1Programacion/tarea1.java
@@ -1,3 +1,10 @@
 public class tarea1 {
-    
+    public static void main(String[] args) {
+        string nombre = "prueba"; 
+        System.out.println(name);
+    }
+    public static void main(int[] args) {
+        int no se = "10"
+        System.out.println(no se);
+    }
 }
